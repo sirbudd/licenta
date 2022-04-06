67d7a711e381874c85496b0e11a768d3fc334319
sirbu <dragos.daniel199@gmail.com>
2022-04-06 23:32:19 +0300

    updates

--- data/prepare_data.sh
+++ data/prepare_data.sh
@@ -15,34 +15,34 @@ else
   PYTHON=python
 fi
 
-echo "** Install requirements"
+#echo "** Install requirements"
 # "gdown" is for downloading files from GoogleDrive
-pip3 install --user gdown > /dev/null
+#pip3 install --user gdown > /dev/null
 
-# make sure to download dataset files to "yolov4_crowdhuman/data/raw/"
-mkdir -p $(dirname $0)/raw
-pushd $(dirname $0)/raw > /dev/null
+# # make sure to download dataset files to "yolov4_crowdhuman/data/raw/"
+# mkdir -p $(dirname $0)/raw
+# pushd $(dirname $0)/raw > /dev/null
 
-get_file()
-{
-  # do download only if the file does not exist
-  if [[ -f $2 ]];  then
-    echo Skipping $2
-  else
-    echo Downloading $2...
-    python3 -m gdown.cli $1
-  fi
-}
+# get_file()
+# {
+#   # do download only if the file does not exist
+#   if [[ -f $2 ]];  then
+#     echo Skipping $2
+#   else
+#     echo Downloading $2...
+#     python3 -m gdown.cli $1
+#   fi
+# }
 
-echo "** Download dataset files"
-get_file https://drive.google.com/uc?id=1sHTitzBm8worH65vEcBk0C8C-rdpbjK5&confirm=t CrowdHuman_train01.zip
-get_file https://drive.google.com/uc?id=1P5u9yh7WOYLR8BZ1WyCiFt86a0M_XTy5&confirm=t CrowdHuman_train02.zip
-get_file https://drive.google.com/uc?id=1BZtyes22dI8wQSmrVhohv-APbvhPkB88&confirm=t CrowdHuman_train03.zip
-get_file https://drive.google.com/uc?id=1p3jDRhKnFjc8VB2odDPLeH-9ccVaQSzr&confirm=t CrowdHuman_val.zip
-# test data is not needed...
-# get_file https://drive.google.com/uc?id=1tQG3E_RrRI4wIGskorLTmDiWHH2okVvk CrowdHuman_test.zip
-get_file https://drive.google.com/uc?id=14njhG0-mOVcvtIH6Dq1vBtd10vVViCZ-&confirm=t annotation_train.odgt
-get_file https://drive.google.com/uc?id=1Gj30JcW9Nwvl3W8IOLD-fV_jzwhfcqxe&confirm=t annotation_val.odgt
+# echo "** Download dataset files"
+# get_file https://drive.google.com/uc?id=1sHTitzBm8worH65vEcBk0C8C-rdpbjK5&confirm=t CrowdHuman_train01.zip
+# get_file https://drive.google.com/uc?id=1P5u9yh7WOYLR8BZ1WyCiFt86a0M_XTy5&confirm=t CrowdHuman_train02.zip
+# get_file https://drive.google.com/uc?id=1BZtyes22dI8wQSmrVhohv-APbvhPkB88&confirm=t CrowdHuman_train03.zip
+# get_file https://drive.google.com/uc?id=1p3jDRhKnFjc8VB2odDPLeH-9ccVaQSzr&confirm=t CrowdHuman_val.zip
+# # test data is not needed...
+# # get_file https://drive.google.com/uc?id=1tQG3E_RrRI4wIGskorLTmDiWHH2okVvk CrowdHuman_test.zip
+# get_file https://drive.google.com/uc?id=14njhG0-mOVcvtIH6Dq1vBtd10vVViCZ-&confirm=t annotation_train.odgt
+# get_file https://drive.google.com/uc?id=1Gj30JcW9Nwvl3W8IOLD-fV_jzwhfcqxe&confirm=t annotation_val.odgt
 
 # unzip image files (ignore CrowdHuman_test.zip for now)
 echo "** Unzip dataset files"
